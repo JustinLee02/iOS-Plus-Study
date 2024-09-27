9d5c923904d646779572c22dd0f05ce810934853
SOOHYUNLEE08 <lsoo0827@gmail.com>
2024-09-27 15:40:43 +0900

    09/27

---
+++ Section11/Learn/Learn/LearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnApp.swift
+//  Learn
+//
+//  Created by 이수현 on 9/27/24.
+//
+
+import SwiftUI
+
+@main
+struct LearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
